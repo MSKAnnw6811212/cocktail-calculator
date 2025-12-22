be7c86712c8f3e8d2bacd227979314fb40ed9895
MSKAnnw6811212 <markus.annweiler@gmail.com>
2025-12-22 12:58:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -641,6 +641,12 @@ if(printBtn) printBtn.addEventListener('click', () => {
 });
 
 initData();
+// -- PWA Service Worker Registration --
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log('Service Worker Registered'))
+    .catch((err) => console.log('SW Failed:', err));
+}
 
 
 
